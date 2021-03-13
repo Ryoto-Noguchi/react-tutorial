35390f2cc691af42ffd3a2caa0b836cf11ecaeb9
Ryoto-Noguchi <ryoto.noguchi@gmail.com>
2021-03-13 19:38:54 +0900

    Full React Course 2020 2:02: 14

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => {
+  return (
+    <img
+      src="https://images-na.ssl-images-amazon.com/images/I/618LsqHnfgL._AC_UL200_SR200,200_.jpg"
+      alt=""
+    />
+  );
+};
+
+const Title = () => {
+  return <h1>How to Catch a Leprechaun</h1>;
+};
+
+const Author = () => {
+  return <h4>Adam Wallace</h4>;
+};
+
+ReactDom.render(<BookList />, document.getElementById("root"));
